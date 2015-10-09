345bb400567a994c1061fe98fd75729edc9dfc0b
Sven Nierlein <sven@nierlein.de>
2015-10-09 10:16:39 +0200

    clean trailing whitespace

--- btrfs-size.sh
+++ btrfs-size.sh
@@ -8,7 +8,7 @@ if [ $1 ]; then
 LOCATION=$1
 fi
 
-OUTPUT="" 
+OUTPUT=""
 
 COL1=`sudo btrfs subvolume list "$LOCATION"`
 COL1=$(echo "$COL1" | cut -c 4-)
@@ -18,16 +18,16 @@ COL2=`sudo btrfs qgroup show "$LOCATION" --raw 2>&1`
 CONTINUE=false
 if [[ $COL2 == *"unrecognized option"* ]]; then
     COL2=`sudo btrfs qgroup show "$LOCATION" `
-fi    
+fi
 
-COL2=$(echo "$COL2" | cut -c 2-) 
- 
+COL2=$(echo "$COL2" | cut -c 2-)
+
 
 
 function convert()
-{ 
+{
         OUT=`echo "$i" | awk '{ sum=$1 ; hum[1024^3]="GB";hum[1024^2]="MB";hum[1024]="KB"; for (x=1024^3; x>=1024; x/=1024){ if (sum>=x) { printf "%.2f%s\n",sum/x,hum[x];break } }}'`
-        OUTPUT=$(printf "%-9s" $OUT) 
+        OUTPUT=$(printf "%-9s" $OUT)
         echo "$OUTPUT"
 }
 
@@ -35,30 +35,30 @@ i=0
 ECL_TOTAL=0
 INDEX=0
 LC_ALL=C
-for i in $COL2; do  
+for i in $COL2; do
     if [[ $i == *"groupid"* ]] || [[ $i == *"----"* ]]; then
         continue;
-    fi 
-    if [[ ! $i =~ ^[A-Za-z-]+$ ]]; then  
-      if [[ "$i" == *\/* ]]; then 
+    fi
+    if [[ ! $i =~ ^[A-Za-z-]+$ ]]; then
+      if [[ "$i" == *\/* ]]; then
         INDEX=0
-        ROWID=$(echo "$i" | cut -c 2-)   
+        ROWID=$(echo "$i" | cut -c 2-)
         OUTPUT+="
-$ROWID  " 
+$ROWID  "
       else
-        ((INDEX++))   
+        ((INDEX++))
         if [ -z `echo $i | tr -d "[:alpha:]"` ]; then
             echo $i" letters\n"
             OUTPUT="$OUTPUT"$(printf "%-9s" $i)
         else
             if [ $INDEX -eq 2 ]; then
-                ECL_TOTAL=$(($i + $ECL_TOTAL)) 
+                ECL_TOTAL=$(($i + $ECL_TOTAL))
             fi
             OUTPUT="$OUTPUT$(convert $i)"
         fi
        fi
     fi
-done  
+done
 
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
 printf "%-67s" "Snapshot / Subvolume"
@@ -73,19 +73,19 @@ IFS=$'\n'
 
 for item in  $COL1; do
     ID=$(echo $item | grep -o '^[0-9.]\+' )
-    for item2 in $OUTPUT; do 
-        ID2=$(echo $item2 | grep -o '^[0-9.]\+' )  
+    for item2 in $OUTPUT; do
+        ID2=$(echo $item2 | grep -o '^[0-9.]\+' )
             if [ "$ID" = "$ID2" ]; then
                 printf "%-64s" $item
                 echo  "   $item2"
                 break;
             fi
     done
-done 
+done
 
 if [ $ECL_TOTAL -gt "1" ]; then
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
     i=$ECL_TOTAL
-    printf "%-64s" " "  
+    printf "%-64s" " "
     printf "Exclusive Total: $(convert $i) \n"
 fi
